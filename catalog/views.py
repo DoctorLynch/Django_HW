5f05094491208c93c460bee5edf12f67ef365a16
DoctorLynch <doctor_linchenko@mail.ru>
2023-09-22 02:13:01 +0300

    HW2

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.template import context
 
 
 def index(request):
-    return render(request, 'catalog/index.html', context=context)
+    return render(request, 'catalog/index.html')
